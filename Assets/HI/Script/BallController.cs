f01d573761774827870b37ada14586088b94bfe2
DESKTOP-GEVROUC\shared <b0122034@edu.kit.ac.jp>
2023-05-16 12:33:58 +0900

    add  error comment  horizontal1

--- Assets/HI/Script/BallController.cs
+++ Assets/HI/Script/BallController.cs
@@ -38,6 +38,8 @@ public class BallController : MonoBehaviour
         // 上記のURLを閲覧すると、1人称視点にはなったが、
         // 迷路などを作製していないので、自分の位置を見失う可能性あり
         // 流れとしては迷路作成してから、1人称視点にしたほうがよさそう
+        // なお一度feature/tanakaにコミット&プルをしてからもう一回実行したら、
+        // ArgumentException Input Axis Horizontal1 is not setupと出てきてエラーとなった
         if(Input.GetAxis("Horizontal1") != 0){
 
             //以下ジョイコンRの場合
