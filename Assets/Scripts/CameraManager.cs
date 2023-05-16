c62eb05398b8551ceb96ba5aed7cabde6f921c5c
fujihara <fu990114@gmail.com>
2023-05-16 17:35:23 +0900

    add: mazestick

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+  Transform tf; //Main CameraのTransform
+  Camera cam; //Main CameraのCamera
+  Rigidbody hrb; //Human（親オブジェクト）のRigidbody
+  
+  void Start()
+  {
+    tf = this.gameObject.GetComponent<Transform>(); //Main CameraのTransformを取得する。
+    cam = this.gameObject.GetComponent<Camera>(); //Main CameraのCameraを取得する。
+    hrb = transform.parent.gameObject.GetComponent<Rigidbody>(); //Human（親オブジェクト）のRigidbodyを取得する。
+  }
+
+  void FixedUpdate()
+  {
+    if(!(Input.GetKey(KeyCode.LeftShift)) && Input.GetKey(KeyCode.UpArrow)) //上キーが押されていれば
+    {
+      hrb.position = hrb.position + (transform.forward*Time.deltaTime*7.0f); //人を前進させる。
+    }
+    else if(!(Input.GetKey(KeyCode.LeftShift)) && Input.GetKey(KeyCode.DownArrow)) //下キーが押されていれば
+    {
+      hrb.position = hrb.position - (transform.forward*Time.deltaTime*7.0f); //人を後ずさりさせる。
+    }
+    if(!(Input.GetKey(KeyCode.LeftShift)) && Input.GetKey(KeyCode.LeftArrow)) //左キーが押されていれば
+    {
+      hrb.position = hrb.position - (transform.right*Time.deltaTime*7.0f); //人を左へカニ歩きさせる。
+    }
+    else if(!(Input.GetKey(KeyCode.LeftShift)) && Input.GetKey(KeyCode.RightArrow)) //右キーが押されていれば
+    {
+      hrb.position = hrb.position + (transform.right*Time.deltaTime*7.0f); //人を右へカニ歩きさせる。
+    }
+    if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.UpArrow)) //左側のShiftと上キーが押されていれば
+    {
+      transform.Rotate(new Vector3(-2.0f,0.0f,0.0f)); //カメラを上へ回転。
+    }
+    else if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.DownArrow)) //左側のShiftと下キーが押されていれば
+    {
+      transform.Rotate(new Vector3(2.0f,0.0f,0.0f)); //カメラを下へ回転。
+    }
+    if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftArrow)) //左側のShiftと左キーが押されていれば
+    {
+      transform.Rotate(new Vector3(0.0f,-2.0f,0.0f)); //カメラを左へ回転。
+    }
+    else if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.RightArrow)) //左側のShiftと右キーが押されていれば
+    {
+      transform.Rotate(new Vector3(0.0f,2.0f,0.0f)); //カメラを右へ回転。
+    }
+    if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.R)) //左側のShiftとRキーが押されていれば
+    {
+      tf.rotation = new Quaternion(0.0f,0.0f,0.0f,0.0f); //カメラの回転をリセットする。
+    }
+  }
+}
---
+++ Assets/Scripts/MazeStick.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeStick : MonoBehaviour
+{
+  public int max_z; //フィールドの縦幅。5以上の奇数にすること。
+  public int max_x; //フィールドの横幅。5以上の奇数にすること。
+  int z; //フィールド配列の縦の要素番号
+  int x; //フィールド配列の横の要素番号
+  int r; //乱数の値
+  Object wall; //壁オブジェクト
+  GameObject wallgo; //壁のゲームオブジェクト
+
+  void Start()
+  {
+    int[,] field = new int[max_z,max_x]; //フィールド（0が通路で、1が壁。）
+    wall = Resources.Load("Wall"); //壁オブジェクトを読み込む。
+    
+    //通路（0）の生成
+    for(z=0; z<max_z; z=z+1) //フィールドの縦幅の分だけループする。
+    {
+      for(x=0; x<max_x; x=x+1) //フィールドの横幅の分だけループする。
+      {
+        field[z,x] = 0;
+      }
+    }
+
+    //上下の外壁（1）の生成
+    for(x=0; x<max_x; x=x+1) //フィールドの横幅の分だけループする。
+    {
+      field[0,x] = 1;
+      field[max_z-1,x] = 1;
+    }
+
+    //左右の外壁（1）の生成
+    for(z=0; z<max_z; z=z+1) //フィールドの縦幅の分だけループする。
+    {
+      field[z,0] = 1;
+      field[z,max_x-1] = 1;
+    }
+
+    //棒倒し法を使った壁（1）の生成（1行めのみ）
+    z = 2; //1行め
+    for(x=2; x<max_x-1; x=x+2) //要素番号xが2から要素番号max_x-1の値まで、1マス飛ばしで棒倒し。
+    {
+      r = Random.Range(1,13); //乱数生成（r = 1から12のランダムな値）
+      field[z,x] = 1; //中心から……
+      if(r<=3) //rが3以下のとき
+      {
+        if(field[z-1,x]==0) //上に棒（壁）がなければ
+        {
+          field[z-1,x] = 1; //上に棒を倒す。
+        }
+        else if(field[z-1,x]==1) //上に棒（壁）があれば
+        {
+          x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+        }
+      }
+      if(r>=4 && r<=6) //rが4から6のとき
+      {
+        if(field[z+1,x]==0) //下に棒（壁）がなければ
+        {
+          field[z+1,x] = 1; //下に棒を倒す。
+        }
+        else if(field[z+1,x]==1) //下に棒（壁）があれば
+        {
+          x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+        }
+      }
+      if(r>=7 && r<=9) //rが7から9のとき
+      {
+        if(field[z,x-1]==0) //左に棒（壁）がなければ
+        {
+          field[z,x-1] = 1; //左に棒を倒す。
+        }
+        else if(field[z,x-1]==1) //左に棒（壁）があれば
+        {
+          x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+        }
+      }
+      if(r>=10) //rが10以上のとき
+      {
+        if(field[z,x+1]==0) //右に棒（壁）がなければ
+        {
+          field[z,x+1] = 1; //右に棒を倒す。
+        }
+        else if(field[z,x+1]==1) //右に棒（壁）があれば
+        {
+          x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+        }
+      }
+    }
+    
+    //棒倒し法を使った壁（1）の生成（2行め以降）
+    for(z=4; z<max_z-1; z=z+2) //zの要素番号4から要素番号max_z-1まで、1マス飛ばしで棒倒し。
+    {
+      for(x=2; x<max_x-1; x=x+2) //xの要素番号2から要素番号max_x-1まで、1マス飛ばしで棒倒し。
+      {
+        r = Random.Range(1,13); //乱数生成（r = 1から12のランダムな値）
+        field[z,x] = 1; //中心から……
+        if(r<=4) //rが4以下のとき
+        {  
+          if(field[z+1,x]==0) //下に棒（壁）がなければ
+          {
+            field[z+1,x] = 1; //下に棒を倒す。
+          }
+          else if(field[z+1,x]==1) //下に棒（壁）があれば
+          {
+            x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+          }
+        }
+        if(r>=5 && r<=8) //rが5から8のとき
+        {
+          if(field[z,x-1]==0) //左に棒（壁）がなければ
+          {
+            field[z,x-1] = 1; //左に棒を倒す。
+          }
+          else if(field[z,x-1]==1) //左に棒（壁）があれば
+          {
+            x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+          }
+        }
+        if(r>=9) //rが9以上のとき
+        {
+          if(field[z,x+1]==0) //右に棒（壁）がなければ
+          {
+            field[z,x+1] = 1; //右に棒を倒す。
+          }
+          else if(field[z,x+1]==1) //右に棒（壁）があれば
+          {
+            x = x - 2; //棒を倒さずに、乱数生成をやり直す。
+          }
+        }
+      }
+    }
+
+    field[0,1] = 0; //スタート地点の壁を撤去する。
+    field[max_z-1,max_x-2] = 0; //ゴール地点の壁を撤去する。
+
+    //壁の配置
+    for(z=0; z<max_z; z=z+1) //フィールドの縦幅の分だけループする。
+    {
+      for(x=0; x<max_x; x=x+1) //フィールドの横幅の分だけループする。
+      {
+        if(field[z,x]==0) //通路なら
+        {
+          //何も配置しない。
+        }
+        else if(field[z,x]==1) //壁なら
+        {
+          wallgo = (GameObject)Instantiate(wall, new Vector3(5.0f*x,5.0f,5.0f*z), Quaternion.identity); //壁を配置する。
+        }
+      }
+    }
+  }
+}
