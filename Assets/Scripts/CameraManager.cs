c389f4b1537552801296bb74562778a52bab5524
DESKTOP-GEVROUC\shared <b0122034@edu.kit.ac.jp>
2023-05-23 16:40:24 +0900

    update but no change about goal scene

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraManager : MonoBehaviour
 {
@@ -161,4 +162,29 @@ public class CameraManager : MonoBehaviour
       tf.rotation = new Quaternion(0.0f,0.0f,0.0f,0.0f); //カメラの回転をリセットする。
     }
   }
+  /*
+  void OnControllerColliderHit(ControllerColliderHit hit){
+    Rigidbody body = hit.collider.attachedRigidbody;
+
+    // no rigidbody
+    if (body == null || body.isKinematic)
+        return;
+
+    // We dont want to push objects below us
+    if (hit.moveDirection.y < -0.3f)
+        return;
+
+    // Calculate push direction from move direction,
+    // we only push objects to the sides never up and down
+    Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+    // If you know how fast your character is trying to move,
+    // then you can also multiply the push velocity by that.
+
+    // Apply the push
+    body.velocity = pushDir * pushPower;
+    //Debug.Log("1");
+    SceneManager.LoadScene("ClearScene");
+  }
+  */
 }
