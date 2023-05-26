e49030e9277d12500792606ce432603e8af56910
fujihara <fu990114@gmail.com>
2023-05-26 18:12:21 +0900

    update: startcontroller

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -8,4 +8,20 @@ public class StartController : MonoBehaviour
     public void OnClickStart(){
        SceneManager.LoadScene("GameScene");
     }
+
+    public void JoystickButton0(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    
+
+    void FixedUpDate(){
+        /*if(Input.GetKey(KeyCode.Joystick1Button10) || Input.GetKey(KeyCode.Joystick2Button10)
+        || Input.GetKey(KeyCode.Joystick1Button11) || Input.GetKey(KeyCode.Joystick2Button11)){
+          //ジョイコンの接続順によってJoystick1とJoystick2が定義される
+          //ジョイコンLのスティックの押し込みの反応を確認
+          Debug.Log("スティック押し込んだ");
+          SceneManager.LoadScene("GameScene");
+        }*/
+    }
 }
