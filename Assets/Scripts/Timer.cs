a350487d5165202ae5ac788a614461d5c0189fea
KeitaNakata <nakata.keita.12@gmail.com>
2023-05-26 16:04:03 +0900

    add: pause and resume

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -24,4 +24,11 @@ public class Timer : MonoBehaviour
         Text uiText = GetComponent<Text> ();
         uiText.text = "Time : " + t;
     }
+
+    public void Pause(){
+        Time.timeScale = 0;
+    }
+    public void Resume(){
+        Time.timeScale = 1;
+    }
 }
