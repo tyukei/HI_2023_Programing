56267ecd34c30d4cb2bac42213aafbcea3640839
KeitaNakata <nakata.keita.12@gmail.com>
2023-05-26 12:57:58 +0900

    add: start scene

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public void OnClickStart(){
+       SceneManager.LoadScene("GameScene");
+    }
+}
