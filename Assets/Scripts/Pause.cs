89a355ea844ee8fe79ad7df4732cb117cad6ed92
KeitaNakata <nakata.keita.12@gmail.com>
2023-05-26 16:16:52 +0900

    add: pause and resume

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -5,5 +5,24 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     [SerializeField] private Timer timer;
+    [SerializeField] private GameObject pausePanel;
     
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            OnClickLeftArrow();
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.LeftArrow)){
+            OnClickLeftRight();
+        }
+    }
+    public void OnClickLeftArrow(){
+        Instantiate(pausePanel);
+        timer.Pause();
+    }
+    public void OnClickLeftRight(){
+        timer.Resume();
+        Destroy(pausePanel);
+    }
+
 }
