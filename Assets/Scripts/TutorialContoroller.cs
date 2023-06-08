352a0dad9d0aa02d866aee494053dd6419ed7f95
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-06-09 07:56:19 +0900

    update: start

--- Assets/Scripts/TutorialContoroller.cs
+++ Assets/Scripts/TutorialContoroller.cs
@@ -7,22 +7,30 @@ public class TutorialContoroller : MonoBehaviour
     [SerializeField] private GameObject tutorial_Canvas;
     [SerializeField] private GameObject tutorial1;
     [SerializeField] private GameObject tutorial2;
-    void Update()
-    {
-        if(KeyDef.Right()){
+
+    void OnEnable(){
+        StartCoroutine(CheckKey());
+    }
+
+    IEnumerator CheckKey(){
+        while(true){
+        if(KeyDef.Right() && tutorial_Canvas.activeSelf == true){
             if(tutorial1.activeSelf){
                 tutorial1.SetActive(false);
                 tutorial2.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
             }
             else{
                 tutorial1.SetActive(true);
                 tutorial2.SetActive(false);
+                yield return new WaitForSeconds(0.5f);
             }
         }
-        if(KeyDef.Left()){
+        if(KeyDef.Left() && tutorial_Canvas.activeSelf == true){
+            yield return new WaitForSeconds(0.5f);
             tutorial_Canvas.SetActive(false);
         }
-
-        
+        yield return null;
+        }
     }
 }
